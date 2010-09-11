a14ce7329745652f3349d732824cbf98d5c0605b
Petr Zaparka <petr.zaparka@unboxedconsulting.com>
2010-09-11 13:27:37 +0100

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+gem 'mysql2'
+
+group :development, :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'cucumber-rails'
+  gem 'cucumber'
+  gem 'rspec-rails'
+  gem 'spork'
+  gem 'launchy'    # So you can do Then show me the page
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThankYouNew::Application.config.secret_token = '942c985591f795a0eaf72223eae84629a2f9612eaf34fd9350b83a49d97529f76991f8222beee04675e064b73354ff4cd2bbdee78bff3d3e9f86e9bab56e8727'
