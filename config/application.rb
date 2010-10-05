3f28acb6314f81c1a8c118d15dae691291add732
Petr Zaparka <petr.zaparka@unboxedconsulting.com>
2010-10-05 17:45:18 +0100

    Test for git notifier.

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module ThankYouNew
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
     config.action_view.javascript_expansions[:defaults] = ["jquery-1.4.2.min", "jquery-ujs/src/rails"]
-    # Configure the default encoding used in templates for Ruby 1.9.
+
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
