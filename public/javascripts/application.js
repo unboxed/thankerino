9de1b4f940eb8d0dc70e23194714540cc45b20f5
Petr Zaparka <petr.zaparka@unboxedconsulting.com>
2010-10-25 19:52:15 +0100

    Add better view for error messages.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
-  divs = "#flash_success, #flash_notice, #flash_error, #flash_alert";
+  divs = "#flash_success, #flash_notice, #flash_error, #flash_alert, #errorExplanation";
   $(divs).fadeIn(1000).fadeOut(10000);
 });
