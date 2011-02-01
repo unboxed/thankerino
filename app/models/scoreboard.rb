16b40d72c65be251ee3bdef85eb33b74cf1897bc
Petr Zaparka <petr.zaparka@unboxedconsulting.com>
2011-02-01 22:02:56 +0000

    Annotation

--- app/models/scoreboard.rb
+++ app/models/scoreboard.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: scoreboards
+#
+#  id         :integer(4)      not null, primary key
+#  name       :string(255)
+#  from_date  :date
+#  to_date    :date
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Scoreboard < ActiveRecord::Base
 end
--- spec/models/scoreboard_spec.rb
+++ spec/models/scoreboard_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: scoreboards
+#
+#  id         :integer(4)      not null, primary key
+#  name       :string(255)
+#  from_date  :date
+#  to_date    :date
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe Scoreboard do
